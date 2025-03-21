1d725d4cd4978a77fbfc0a37d5db41d74868835c
/\/\@SoOd <42731850+masoodMEH@users.noreply.github.com>
2025-03-22 01:11:55 +0330

    initial commit

---
+++ BlazorShop.Server/webpack.config.js
@@ -0,0 +1,18 @@
+﻿const path = require("path");
+
+module.exports = {
+    entry: "./wwwroot/js/main.js",
+    output: {
+        path: path.resolve(__dirname, "wwwroot"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader", "postcss-loader"],
+            },
+        ],
+    },
+    mode: "development",
+};
